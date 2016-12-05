c7d7f986cb0e6c4889d68db78ebf55438cb58695
CaptainAtlas <daniel.bye@colorado.edu>
2016-12-04 18:11:23 -0700

    periodic updates

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -5,12 +5,12 @@ public class Driver extends TimerTask {
         GetProcessList processes = new GetProcessList();
         processes.updateProcesses();
     }
-    public static void main (String[]args){
-
-
-        NodeProc myNode = new NodeProc();
-        myNode.setPid(1);
-        System.out.println(myNode.getRamUsage());
-
-    }
+//    public static void main (String[]args){
+//
+//
+//        NodeProc myNode = new NodeProc();
+//        myNode.setPid(1);
+//        System.out.println(myNode.getRamUsage());
+//
+//    }
 }
--- Project/src/SchedulerMain.java
+++ Project/src/SchedulerMain.java
@@ -1,2 +1,20 @@
+import java.util.Timer;
+
 public class SchedulerMain {
+    public static void main(String[] args) throws InterruptedException {
+        Timer update = new Timer();
+        Driver ProcData = new Driver();
+        update.schedule(ProcData, 0, 10000);
+
+        for (int i = 0; i <= 5; i++){
+            System.out.println("Updating...");
+            Thread.sleep(11000);
+            if (i == 5){
+                System.out.println("Stop Testing");
+                System.exit(0);
+            }
+        }
+
+
+    }
 }
